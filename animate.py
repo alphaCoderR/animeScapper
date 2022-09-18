a9d98a18f606bf34515833c1829b148bbb61a303
alphaCoderR <eldrago2000@gmail.com>
2022-09-18 21:26:22 +0530

    Animations Added

---
+++ animate.py
@@ -0,0 +1,5 @@
+from halo import Halo
+
+search_text = "Searching... Please Wait !!!"
+search_loader = Halo(text=search_text, text_color="white",
+              color="magenta", spinner='dots', animation="marquee")
--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import re
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 import urllib.parse
+from animate import (search_loader)
+
 pd.set_option('display.max_colwidth', 500)
 
 
 def searchModule(animeName):
+    search_loader.start()
+    global searchList
     searchList = []
     page = requests.get("https://kayoanime.com/?s="+animeName)
     soup = bs(page.content, 'html.parser')
@@ -20,9 +24,14 @@ def searchModule(animeName):
         link = link.contents[0].get('href')
         option = dict({"Name": name, "Link": link})
         searchList.append(option)
+        if index == 0:
+            search_loader.stop()
         print(index+1, name)
 
 
 animeName = urllib.parse.quote(input("Enter the name of the anime : "))
 searchModule(animeName)
-# print(searchList)
+choice = int(input("Enter your choice : "))
+page = requests.get(searchList[choice-1]["Link"])
+print(page)
+# print(searchList[0]["Link"])
